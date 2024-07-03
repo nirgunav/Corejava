479bc142793a3e5ce160f20fa264b8fc4554cb39
nirgu <nirgu@NIRGUNAVALVEAKR>
2024-07-03 21:57:22 +0530

    commit message

---
+++ Corejavaprogram/src/com/tnsif/encapsulation/Encapsulationdemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.encapsulation;
+//demo for encapsulation
+
+public class Encapsulationdemo {
+	int serialnumber;
+	String name;
+	int age;               //data members
+	
+	void show() {          //member function
+      System.out.println("serialnumber "+serialnumber +" "+ "name "+name+"age "+age);
+		
+      
+	}
+	
+
+}
