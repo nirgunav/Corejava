e2804d1ac3c92e214ddd6ee7bff0151c55f8c4a2
nirgu <nirgu@NIRGUNAVALVEAKR>
2024-07-03 14:03:02 +0530

    commit message

---
+++ Corejavaprogram/src/com/tnsif/packages/Publicdemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.packages;
+
+public class Publicdemo {
+	
+	public int a=8;
+	
+	public static void main(String[] args) {
+		Publicdemo d =new Publicdemo();
+		
+		System.out.println(d.a);
+	}
+
+}
