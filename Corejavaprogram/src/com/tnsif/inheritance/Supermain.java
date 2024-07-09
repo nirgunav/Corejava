38d255ad34cda7c30adb4691519e8356c92b2db6
nirgu <nirgu@NIRGUNA-PINKU.Dlink>
2024-07-09 10:07:51 +0530

    commit message# WARNING: head commit changed in the meantime
    commit message

---
+++ Corejavaprogram/src/com/tnsif/inheritance/Supermain.java
@@ -0,0 +1,13 @@
+package com.tnsif.inheritance;
+
+public class Supermain {
+	
+	public static void main(String[] args) {
+		Student s=new Student("nirguna" , 655236969, "banglore", 984509434);
+		System.out.println(s);
+		
+		Student s1=new Student(1,"sriindu");
+		System.out.println(s1);
+	}
+
+}
---
+++ Corejavaprogram/src/com/tnsif/superkeyword/Superex.java
@@ -0,0 +1,14 @@
+package com.tnsif.superkeyword;
+//parent class
+public class Superex{
+	
+	int a=20;
+	
+	void drinking() {
+		System.out.println("coffee");
+	}
+	Superex(){
+		System.out.println("hello");
+	}
+}
+
