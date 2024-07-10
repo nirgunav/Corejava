eef94038dde7165019bf732da54dae1c74300ab8
nirgu <nirgu@NIRGUNA-PINKU>
2024-07-10 09:54:48 +0530

    commit message

---
+++ Corejavaprogram/src/com/tnsif/interfacedemo/Interfacedemo.java
@@ -0,0 +1,6 @@
+package com.tnsif.interfacedemo;
+
+public interface Interfacedemo {
+	void print();
+
+}
---
+++ Corejavaprogram/src/com/tnsif/interfacedemo/Myinterface.java
@@ -0,0 +1,20 @@
+package com.tnsif.interfacedemo;
+//nested interface
+
+public interface Myinterface {
+	
+	void calculatearea();
+	
+	// nested interface
+	interface MYinnerinterface{
+		int id=20;
+		void print();
+		
+	}
+
+}
+
+
+
+
+
