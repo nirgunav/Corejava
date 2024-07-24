b4c953a9f33c96075bffbe16ddcd11ce58b38090
nirgu <nirgu@NIRGUNA-PINKU.Dlink>
2024-07-24 12:25:14 +0530

    commit message

---
+++ Corejavaprogram/src/Numberfunction.java
@@ -0,0 +1,19 @@
+
+public class Numberfunction {
+	public  static int getFactorial(int n) {
+		int f=1;
+		while(n!=0) {
+			f=f*n;
+			if(n<0) {
+				n++;
+			}
+				else {
+					n--;
+				}
+			}
+			return f;
+		}
+	}
+
+
+
